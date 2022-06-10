902865b0c09a4df7547f6c0efcdf8304f1bfa417
JessicaBorja <jessicalizethborja@gmail.com>
2022-06-10 22:29:05 +0200

    multi evaluations error

--- run_on_cluster/slurm_eval.py
+++ run_on_cluster/slurm_eval.py
@@ -95,8 +95,8 @@ def main():
         epoch_args = [",".join(arr) for arr in split_epochs if len(arr)]
         for epoch_arg in epoch_args:
             # cmd = [(training_dir / "evaluate.sh").as_posix(), "--checkpoints", epoch_arg]
-            script += f"--checkpoints {epoch_arg}"
-            submit_job(job_opts, script)
+            _script = script + f"--checkpoints {epoch_arg}"
+            submit_job(job_opts, _script)
     else:
         submit_job(job_opts, script)
         
